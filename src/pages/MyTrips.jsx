7b764fe0ee8f3e2a2c9698bbb7edd6913dd07d0f
beehyv <beehyv@bhlp0006.router6986fa.com>
2024-07-19 17:02:46 +0530

    New updated

--- src/pages/MyTrips.jsx
+++ src/pages/MyTrips.jsx
@@ -2,15 +2,17 @@ import Navbar from "../components/Navbar";
 import Main from "../components/HotelMain";
 import Card from "../components/Cards"
 import Footer from "../components/Footer"
+import MyTrip from "../components/MyTrips"
 
 export default function MyTrips() {
   return (
-    <div className="relative bg-[#fffff] ">
+    <div className="relative bg-[#fffff]">
       <div className="absolute top-0 left-0 w-full z-50">
         <Navbar />
       </div>
-      <Main />
-     <Card/>
+      {/* <Main />
+     <Card/> */}
+     <MyTrip  />
 
       <Footer></Footer>
     </div>
